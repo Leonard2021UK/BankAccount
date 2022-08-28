a2aab4737fa3cbd41636d3eb80d962b767fb647c
leo <nema.otto@gmail.com>
2022-08-28 21:46:31 +0100

    Test change to start pipeline

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -58,7 +58,7 @@ namespace BankAccountNS
 
         public static void Main()
         {
-            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            BankAccount ba = new BankAccount("Mr. Bryan Walton Test", 11.99);
 
             ba.Credit(5.77);
             ba.Debit(11.22);
